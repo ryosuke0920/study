6f380a7b7f2d86a04e0194b6cf8f0724ebcc4cfb
Ryosuke Ohta <mailryosuke@gmail.com>
2022-05-13 23:18:53 +0900

    Added scope test.

--- php1/test.php
+++ php1/test.php
@@ -12,3 +12,29 @@ test1(null);
 test1();
 test1('');
 test1(0);
+
+class Main
+{
+    public function init()
+    {
+        $sub = new Sub();
+        $this->sep = $sep = "-";
+
+        $sub->loop(function ($index) use ($sep) {
+            echo $index . $sep . $this->sep;
+        });
+    }
+}
+
+class Sub
+{
+    public function loop($func)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $func($i);
+        }
+    }
+}
+
+$main = new Main();
+$main->init();
