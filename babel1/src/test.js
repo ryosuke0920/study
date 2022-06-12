744a59cd26787581aa17ce90ad78b02a0dca0b9c
Ryosuke Ohta <mailryosuke@gmail.com>
2022-06-12 22:47:03 +0900

    Added babel polyfill

--- babel1/src/test.js
+++ babel1/src/test.js
@@ -1,3 +1,7 @@
-import './hoge.js';
-
+import "core-js/stable";
 const fn = () => 1;
+// const fn2 = async () => 1;
+// const ret = await fun2();
+// console.log(ret);
+
+Promise.resolve().finally();
